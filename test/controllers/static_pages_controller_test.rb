6782047da00adc3ce5691e0b1d0d314db6f01821
DenysVinegod <denys.vinegod@gmail.com>
2021-01-29 12:36:42 +0200

    Using layouts, refactoring code.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Melight"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Melight"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Melight"
   end
 
 end
